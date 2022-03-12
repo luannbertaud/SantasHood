80d5548e95463559eb2953a6dd16213246e74bef
Luann <luann.bertaud@epitech.eu>
2022-03-12 13:10:04 +0100

    WORKER: WIP: Preparing multidimensional clustering

--- worker/computation/clusters.py
+++ worker/computation/clusters.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
 
-from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN, AgglomerativeClustering, KMeans
 from sklearn.metrics import silhouette_score
-from sklearn.preprocessing import MinMaxScaler
+from hdbscan import HDBSCAN
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def compute_clusters(gifts, n_clusters=None):
-    scaler = MinMaxScaler()
-    scaled_gifts = scaler.fit_transform(gifts)
     
     if (not n_clusters):
         scores = []
-        record = (-1, 2)
-        cluster_nb_list = range(2, len(scaled_gifts))
+        record = (-1, 2, -1)
+        cluster_nb_list = range(2, len(gifts)+1)
         for cn in cluster_nb_list:
-            agc = AgglomerativeClustering(n_clusters=cn)
-            agc.fit(scaled_gifts)
-            score = silhouette_score(scaled_gifts, agc.labels_)
+            # agc = KMeans(n_clusters=cn, algorithm="k-mean++")
+            agc = HDBSCAN(min_cluster_size=cn)
+            agc.fit(gifts)
+            if (len(np.unique(agc.labels_)) <= 1) or (len(np.unique(agc.labels_)) >= len(gifts)):
+                scores.append(0)
+                continue
+            score = (silhouette_score(gifts, agc.labels_))
             scores.append(score)
-            if (score > record[0]):
-                record = (score, cn)
+            if (score >= record[0]):
+                record = (score, cn, len(np.unique(agc.labels_)))
 
         n_clusters = record[1] #TODO record is maybe not the best one
-        print(f"For {len(gifts)} gifts, the optimal clusters number seems to be {n_clusters}.")
+        print(f"For {len(gifts)} gifts, the optimal clusters number seems to be {record[2]} (min {record[1]}) -> {record[0]}.")
+        print(scores)
 
         # plt.style.use("fivethirtyeight")
         # plt.plot(cluster_nb_list, scores)
@@ -31,6 +36,7 @@ def compute_clusters(gifts, n_clusters=None):
         # plt.ylabel("Silhouette Coefficient")
         # plt.show()
 
-    agc = AgglomerativeClustering(n_clusters=n_clusters)
-    agc.fit(scaled_gifts)
+    # agc = AgglomerativeClustering(n_clusters=n_clusters)
+    agc= HDBSCAN(min_cluster_size=n_clusters)
+    agc.fit(gifts)
     return agc.labels_, agc
--- worker/worker.py
+++ worker/worker.py
@@ -3,9 +3,11 @@
 import sys
 import random
 import numpy as np
+from copy import deepcopy
+from computation.clusters import compute_clusters
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler
 from tools.db import validateDatabase
 from tools.graphical import show_clusters
-from computation.clusters import compute_clusters
 
 
 def test_clustering():
@@ -17,14 +19,103 @@ def test_clustering():
             tmp.append(random.randint(0, 100))
         features = np.append(features, [tmp], axis=0)
 
-    labels, _ = compute_clusters(features)
+    scaler = MinMaxScaler()
+    gifts = scaler.fit_transform(features)
+    pprint(gifts)
+    labels, _ = compute_clusters(gifts)
+    pprint(labels)
     show_clusters(labels, features)
 
+
+def encode_giftcards(cards):
+    all_scopes = []
+    all_categories = []
+
+    for card in cards:
+        if (card["scope"] not in all_scopes):
+            all_scopes.append(card["scope"])
+        for cat in card["categories"]:
+            if (cat not in all_categories):
+                all_categories.append(cat)
+
+    encoded = []
+    for card in cards:
+        buff = []
+        # buff.append(0)
+        buff.append(card["budget"])
+        # buff.append(card["cluttering"])
+        # buff.append(0 if not card["shortlived"] else 1)
+        buff.append(all_scopes.index(card["scope"]))
+        for cat in card["categories"]:
+            buff.append(all_categories.index(cat))
+            encoded.append(buff)
+            buff = buff[:-1]
+    
+    scaler = StandardScaler()
+    encoded = scaler.fit_transform(encoded)
+    return encoded
+
+
+
+data = {
+        "cards": [
+        # {
+        #     "uuid": "999",
+        #     "name": "FirstaOne",
+        #     "description": "descOne",
+        #     "budget": 100,
+        #     "scope": "Family",
+        #     "cluttering": 9,
+        #     "shortlived": False,
+        #     "categories": ["dodo", "bato", "cado"]
+        # },
+        # {
+        #     "uuid": "9999",
+        #     "name": "Secondone",
+        #     "description": "descTwo",
+        #     "budget": 10,
+        #     "scope": "Personal",
+        #     "cluttering": 1,
+        #     "shortlived": True,
+        #     "categories": ["nature", "sport", "dodo"]
+        # }
+    ]
+}
+
+cats = ["nature", "sport", "dodo"]
+scopes = ["Personal", "Family", "Tryue"]
+for i in range(200):
+    data["cards"].append(
+    {
+        "uuid": "999",
+        "name": "FirstaOne",
+        "description": "descOne",
+        "budget": random.randint(10, 1000),
+        "scope": scopes[random.randint(0, len(scopes)-1)],
+        "cluttering": 1,
+        "shortlived": False,
+        "categories": [cats[random.randint(0, len(cats)-1)]]
+    })
+    
 
 if __name__ == "__main__":
     if ((len(sys.argv) > 1) and (sys.argv[1] == "--ping")):
         print("pong")
         exit(0)
     
-    validateDatabase()
-    test_clustering()
+    # validateDatabase()
+    # test_clustering()
+
+from pprint import pprint
+
+encoded = encode_giftcards(data["cards"])
+
+pprint(encoded)
+labels, _ = compute_clusters(encoded)
+pprint(labels)
+show_clusters(labels, np.array(encoded))
+# test_clustering()
+
+for i in range(len(data["cards"])):
+    if (labels[i] == 1):
+        pprint(data["cards"][i])
