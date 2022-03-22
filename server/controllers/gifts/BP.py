3513a0cbb207c416198a77d01b18417e2811c08b
Luann <luann.bertaud@epitech.eu>
2022-03-22 12:09:44 +0100

    WORKER: calculating cost for closest user

--- server/controllers/gifts/BP.py
+++ server/controllers/gifts/BP.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+import base64
+import json
 from flask import Blueprint, request
 from tools.db import needs_db
 from controllers.gifts.save_cards import save_giftcard
+
+from models.db import UserCards
+from peewee import Case, Expression
+from playhouse.postgres_ext import fn, ArrayField
 
 giftsBP = Blueprint('giftsBP', __name__)
 
@@ -44,3 +50,32 @@ def users_newcards():
     if (len(updated) != len(data['cards'])):
         return {"code": 400, "message": f"Not all cards could be updated: {', '.join(failed)}"}, 400
     return {"code": 200, "message": f"Updated cards: {', '.join(updated)}"}
+
+
+def __get_cost(bage, bsexe, binterests):
+
+    age = fn.ABS(UserCards.age - bage)
+    sexe = (0 if UserCards.sexe == bsexe else 1)
+    interests = Expression(len(binterests), '-', fn.cardinality(UserCards.interests.cast("text[]")))
+    res = age + sexe + interests
+    return res
+
+@giftsBP.route("/searchfor", methods=["GET"])
+@needs_db
+def users_searchfor():
+    usercard = None
+    try:
+        usercard = json.loads(base64.urlsafe_b64decode(request.args["usercard"]))
+    except:
+        return {"code": 400, "message": "Invalid usercard argument."}, 400
+    if not usercard:
+        return {"code": 400, "message": "Invalid usercard argument."}, 400
+
+
+    cost = Case(None, (
+        (True, __get_cost(usercard["age"], usercard["sexe"], usercard["interests"])),
+    ), -1)
+    query = UserCards.select(UserCards.age, cost.alias('cost'))
+    for q in query.dicts():
+        print(q)
+    return usercard
