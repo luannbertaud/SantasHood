2628eab15dd907fa5f8f11c2f8bee0ae7af1ecbb
Luann <luann.bertaud@epitech.eu>
2022-03-26 14:28:38 +0100

    WEB: Fixing TransferList component

--- web/src/components/Categories.js
+++ web/src/components/Categories.js
@@ -8,6 +8,8 @@ import {
     ListItem,
     ListItemText,
     ListItemIcon,
+    ListItemAvatar,
+    Avatar,
     Checkbox,
     Button,
     Divider,
@@ -31,11 +33,13 @@ export class TransferList extends React.Component {
         super(props)
         this.state = {
             checked: [],
-            left: [0, 1, 2, 3],
+            left: [0, 1, 2, 3, 8, 8, 8, 8, 8, 8],
             right: [4, 5, 6, 7],
         }
         this.fullHeight = props.fullHeight;
         this.fullWidth = props.fullWidth;
+        this.fontSize = props.fontSize;
+        this.fontSizeSecondary = props.fontSizeSecondary;
         this.handleToggle = this.handleToggle.bind(this);
         this.handleToggleAll = this.handleToggleAll.bind(this);
         this.numberOfChecked = this.numberOfChecked.bind(this);
@@ -85,67 +89,92 @@ export class TransferList extends React.Component {
     };
 
     customList = (title, items) => (
-        <Card
+        <Box 
             sx={{
                 overflow: "auto",
+                maxHeight: "100%",
+                maxWidth: "100%",
                 height: "100%",
                 width: "100%",
+                minWidth: 0,
+                minheight: 0,
             }}
         >
-            <CardHeader
-                sx={{ px: 2, py: 1 }}
-                avatar={
-                    <Checkbox
-                        onClick={this.handleToggleAll(items)}
-                        checked={this.numberOfChecked(items) === items.length && items.length !== 0}
-                        indeterminate={
-                            this.numberOfChecked(items) !== items.length && this.numberOfChecked(items) !== 0
-                        }
-                        disabled={items.length === 0}
-                        inputProps={{
-                            'aria-label': 'all items selected',
-                        }}
-                    />
-                }
-                title={title}
-                subheader={`${this.numberOfChecked(items)}/${items.length} selected`}
-            />
-            <Divider />
-            <List
+            <Card
                 sx={{
-                    bgcolor: 'background.paper',
-                    overflow: 'auto',
+                    minWidth: 0,
+                    minheight: 0,
+                    padding: "3%",
+                    my: "3%",
                 }}
-                dense
-                component="div"
-                role="list"
             >
-                {items.map((value) => {
-                    let labelId = `transfer-list-all-item-${value}-label`;
+                <CardHeader
+                    sx={{ px: 1, py: 1 }}
+                    avatar={
+                        <Checkbox
+                            onClick={this.handleToggleAll(items)}
+                            checked={this.numberOfChecked(items) === items.length && items.length !== 0}
+                            indeterminate={
+                                this.numberOfChecked(items) !== items.length && this.numberOfChecked(items) !== 0
+                            }
+                            disabled={items.length === 0}
+                            inputProps={{
+                                'aria-label': 'all items selected',
+                            }}
+                            sx={{
+                                '& .MuiSvgIcon-root': { fontSize: this.fontSize },
+                                padding: 0,
+                            }}
+                        />
+                    }
+                    title={title}
+                    titleTypographyProps={{fontSize: this.fontSize }}
+                    subheader={`${this.numberOfChecked(items)}/${items.length} selected`}
+                    subheaderTypographyProps={{fontSize: this.fontSizeSecondary }}
+                />
+                <Divider />
+                <List
+                    sx={{
+                        bgcolor: 'background.paper',
+                        overflow: 'auto',
+                    }}
+                    dense
+                    component="div"
+                    role="list"
+                >
+                    {items.map((value) => {
+                        let labelId = `transfer-list-all-item-${value}-label`;
 
-                    return (
-                    <ListItem
-                        key={value}
-                        role="listitem"
-                        button
-                        onClick={this.handleToggle(value)}
-                    >
-                        <ListItemIcon>
-                            <Checkbox
-                                checked={this.state.checked.indexOf(value) !== -1}
-                                tabIndex={-1}
-                                disableRipple
-                                inputProps={{
-                                'aria-labelledby': labelId,
-                                }}
-                            />
-                        </ListItemIcon>
-                        <ListItemText id={labelId} primary={`List item ${value + 1}`} />
-                    </ListItem>
-                    );
-                })}
-            </List>
-        </Card>
+                        return (
+                        <ListItem
+                            key={value}
+                            role="listitem"
+                            button
+                            onClick={this.handleToggle(value)}
+                            sx={{ width: "100%", height: "100%", textAlign: "center" }}
+                            dense
+                            disablePadding
+                        >
+                            <ListItemIcon dense alignItemsFlexStart sx={{ minWidth: 0}} >
+                                <Checkbox
+                                    checked={this.state.checked.indexOf(value) !== -1}
+                                    tabIndex={-1}
+                                    disableRipple
+                                    inputProps={{
+                                    'aria-labelledby': labelId,
+                                    }}
+                                    sx={{
+                                        '& .MuiSvgIcon-root': { fontSize: this.fontSize }
+                                    }}
+                                />
+                            </ListItemIcon>
+                            <ListItemText id={labelId} primary={`List item ${value + 1}`} primaryTypographyProps={{fontSize: this.fontSize}} sx={{ pr: 1 }} />
+                        </ListItem>
+                        );
+                    })}
+                </List>
+            </Card>
+        </Box>
     );
 
     render(){
@@ -163,20 +192,19 @@ export class TransferList extends React.Component {
                     justifyContent: "center",
                     padding: 0,
                     margin: 0,
-                    overflow: "visible",
+                    // overflow: "visivl",
                     height: this.fullHeight,
                     width: this.fullWidth,
                 }}
             >
-                <Grid container spacing={0} sx={{ height: "100%", width: "100%", justifyContent: "center", alignItems: "center", flexWrap: "nowrap", padding: 0 }}>
-                <Grid item xs={5} sx={{ height: "100%", width: "100%" }} >{this.customList('Choices', this.state.left)}</Grid>
-                <Grid item xs={2} sx={{ height: "100%", width: "100%" }} >
-                    <Grid container sx={{ height: "100%", width: "100%", direction: "column", alignItems: "center", justifyContent: "center", display: "flex" }}>
-                        <Box sx={{ padding: "15%" }} >
+                <Grid container spacing={0} sx={{ height: "100%", width: "100%", minWidth: 0, justifyContent: "center", alignItems: "center", flexWrap: "nowrap", padding: 0 }}>
+                <Grid item xs={5} sx={{ height: "100%", width: "100%", minWidth: 0, display: "flex" }} >{this.customList('Choices', this.state.left)}</Grid>
+                <Grid item xs={2} sx={{ height: "100%", width: "100%", minWidth: 0, display: "flex" }} >
+                    <Grid container sx={{ height: "100%", width: "100%", alignItems: "center", justifyContent: "center", display: "flex", overflowY: "auto", overflowX: "hidden" }}>
+                        <Box sx={{ padding: "25%", minWidth: "100%", alignItems: "center", display: "flex", flexDirection: "column" }} >
                             <Button
-                                fullwidth
                                 sx={
-                                    { my: 0.5, width: "100%", minWidth: "0px", backgroundColor: "rgba(255, 0, 140, 0.3)",
+                                    { my: 0.5, width: "50%", minWidth: "0px", backgroundColor: "rgba(255, 0, 140, 0.3)",
                                         "&.MuiButton-outlined": {
                                             color: "rgba(0, 0, 0, 0.4)",
                                             fontSize: "14px"
@@ -192,7 +220,7 @@ export class TransferList extends React.Component {
                             </Button>
                             <Button
                                 sx={
-                                    { my: 0.5, width: "100%", minWidth: "0px", backgroundColor: "rgba(255, 0, 140, 0.3)",
+                                    { my: 0.5, width: "50%", minWidth: "0px", backgroundColor: "rgba(255, 0, 140, 0.3)",
                                         "&.MuiButton-outlined": {
                                             color: "rgba(0, 0, 0, 0.4)",
                                             fontSize: "14px"
@@ -209,7 +237,7 @@ export class TransferList extends React.Component {
                         </Box>
                     </Grid>
                 </Grid>
-                <Grid item xs={5} sx={{ height: "100%", width: "100%" }} >{this.customList('Chosen', this.state.right)}</Grid>
+                <Grid item xs={5} sx={{ height: "100%", width: "100%", minWidth: 0, display: "flex" }} >{this.customList('Chosen', this.state.right)}</Grid>
                 </Grid>
             </Box>
         );
